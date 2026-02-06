be8db3f6e85b00b22a2675d23e7db4cdd5fdba58
Léon Silva Erhard <l.erhard@gmail.com>
2026-02-05 22:23:15 -0300

    feat: add spring boot starter for portal files api

---
+++ java/portal-files-api/src/main/java/com/leonerhard/portal_files_api/PortalFilesApiApplication.java
@@ -0,0 +1,13 @@
+package com.leonerhard.portal_files_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalFilesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalFilesApiApplication.class, args);
+	}
+
+}
---
+++ java/portal-files-api/src/test/java/com/leonerhard/portal_files_api/PortalFilesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonerhard.portal_files_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalFilesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
