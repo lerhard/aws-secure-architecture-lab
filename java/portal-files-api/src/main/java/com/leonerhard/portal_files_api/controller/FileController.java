7cd64a3621469c1799179c188df4959bd1da86da
Léon Silva Erhard <l.erhard@gmail.com>
2026-02-05 23:10:49 -0300

    feat(java): scaffold aws s3 integration components

--- java/portal-files-api/src/main/java/com/leonerhard/portal_files_api/controller/FileController.java
+++ java/portal-files-api/src/main/java/com/leonerhard/portal_files_api/controller/FileController.java
@@ -1,4 +1,24 @@
 package com.leonerhard.portal_files_api.controller;
 
+import com.leonerhard.portal_files_api.service.S3FileService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/files")
 public class FileController {
+
+    private final S3FileService s3FileService;
+
+    public FileController(S3FileService s3FileService) {
+        this.s3FileService = s3FileService;
+    }
+
+    @GetMapping
+    public List<String> listFiles(){
+        return s3FileService.listFiles();
+    }
 }
--- java/portal-files-api/src/main/java/com/leonerhard/portal_files_api/service/S3FileService.java
+++ java/portal-files-api/src/main/java/com/leonerhard/portal_files_api/service/S3FileService.java
@@ -1,4 +1,30 @@
 package com.leonerhard.portal_files_api.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.S3Object;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class S3FileService {
+
+    private final S3Client s3Client;
+
+    @Value("${portal.bucket}")
+    private String bucket;
+
+    public S3FileService(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public List<String> listFiles(){
+        return s3Client.listObjects(b -> b.bucket(bucket)).contents().stream()
+                .map(S3Object::key)
+                .collect(Collectors.toList());
+    }
+
+
 }
